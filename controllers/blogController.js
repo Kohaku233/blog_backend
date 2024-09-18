c00a4cd0efe346b6b3d0a32d1d76cec80ec12710
Kohaku233 <2427503528@qq.com>
2024-09-18 23:45:23 +0800

    根据修改内容创建 commit 并且提交远程

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,7 +4,7 @@ const db = require('../models/db');
 // 创建博客
 exports.createBlog = (req, res) => {
   const { title, content } = req.body;
-  const sql = 'INSERT INTO blog_db (title, content) VALUES (?, ?)';
+  const sql = 'INSERT INTO blogs (title, content) VALUES (?, ?)';
   db.query(sql, [title, content], (err, result) => {
     if (err) {
       console.error('Error creating blog:', err);
@@ -16,7 +16,7 @@ exports.createBlog = (req, res) => {
 
 // 获取所有博客
 exports.getAllBlogs = (req, res) => {
-  const sql = 'SELECT * FROM blog_db';
+  const sql = 'SELECT * FROM blogs';
   db.query(sql, (err, results) => {
     if (err) {
       console.error('Error fetching blogs:', err);
@@ -28,7 +28,7 @@ exports.getAllBlogs = (req, res) => {
 
 // 获取单个博客
 exports.getBlogById = (req, res) => {
-  const sql = 'SELECT * FROM blog_db WHERE id = ?';
+  const sql = 'SELECT * FROM blogs WHERE id = ?';
   db.query(sql, [req.params.id], (err, result) => {
     if (err) {
       console.error('Error fetching blog:', err);
@@ -45,7 +45,7 @@ exports.getBlogById = (req, res) => {
 // 更新博客
 exports.updateBlog = (req, res) => {
   const { title, content } = req.body;
-  const sql = 'UPDATE blog_db SET title = ?, content = ?, updated_at = NOW() WHERE id = ?';
+  const sql = 'UPDATE blogs SET title = ?, content = ?, updated_at = NOW() WHERE id = ?';
   db.query(sql, [title, content, req.params.id], (err, result) => {
     if (err) {
       console.error('Error updating blog:', err);
@@ -61,7 +61,7 @@ exports.updateBlog = (req, res) => {
 
 // 删除博客
 exports.deleteBlog = (req, res) => {
-  const sql = 'DELETE FROM blog_db WHERE id = ?';
+  const sql = 'DELETE FROM blogs WHERE id = ?';
   db.query(sql, [req.params.id], (err, result) => {
     if (err) {
       console.error('Error deleting blog:', err);
