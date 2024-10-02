63db0a0f7ea747ec2b50ff815e000191987ca9a6
Kohaku233 <2427503528@qq.com>
2024-10-02 23:50:50 +0900

    添加 cors 解决跨域问题

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const blogRoutes = require("./routes/blogRoutes");
 
 // 使用中间件
+app.use(cors()); // 添加这行
 app.use(express.json());
 
 // 使用博客路由
@@ -11,5 +13,5 @@ app.use("/api", blogRoutes);
 // 启动服务器
 const PORT = process.env.PORT || 2333;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`服务器正在端口 ${PORT} 上运行`);
 });
