a855c2f89e29369b5a1093d8de70cd72dfd28116
Kohaku233 <2427503528@qq.com>
2024-10-22 23:39:06 +0900

    可以读取 env

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,8 @@
 import { Sequelize } from 'sequelize-typescript';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config(); // 添加这一行以读取 .env 文件
 
 const sequelize = new Sequelize({
   database: process.env.DB_NAME,
